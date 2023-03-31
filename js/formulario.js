a35d9d0172818c25159a02d6a7295bcc86affb97
wcaj <gamezerca@gmail.com>
2023-03-31 18:03:57 -0500

    first commit

---
+++ js/formulario.js
@@ -0,0 +1,9 @@
+function go(){
+
+	if (document.form.password.value=='1234' && document.form.login.value=='Usuario'){ 
+			document.form.submit(); 
+		} 
+		else{ 
+			 alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+		} 
+} 
